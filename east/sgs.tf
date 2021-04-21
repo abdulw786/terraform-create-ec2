5af9cf8a543c26b8c864df0a34bdd0dc2e4ae21c
Sam Smith <sam@example.com>
2021-04-20 20:48:43 -0400

    added quote'

--- east/sgs.tf
+++ east/sgs.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "default" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
