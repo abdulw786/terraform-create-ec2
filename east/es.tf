d783fa96b8a5ef86d9e53b07ac1e7218932a20e7
Sam Smith <sam@example.com>
2021-04-21 21:33:47 -0400

    removed list

--- east/es.tf
+++ east/es.tf
@@ -2,7 +2,7 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
-data "aws_subnet_ids" "default" {
+data "aws_subnet" "default" {
   vpc_id = data.aws_vpc.default.id
   tags = {
     Tier = "private"
@@ -18,7 +18,7 @@ resource "aws_elasticsearch_domain" "es" {
   }
 
   vpc_options {
-    subnet_ids = data.aws_subnet_ids.default.id 
+    subnet_ids = data.aws_subnet.default.id 
 
     security_group_ids = [aws_security_group.default.id]
   }
