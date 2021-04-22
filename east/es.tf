f0c6358f53e8976d11ebe2c89a44a02ff19046e3
Sam Smith <sam@example.com>
2021-04-21 21:23:12 -0400

    added data

--- east/es.tf
+++ east/es.tf
@@ -5,7 +5,7 @@ data "aws_caller_identity" "current" {}
 data "aws_vpc" "default" {}
 
 data "aws_subnet_ids" "default" {
-  vpc_id = "${data.aws_vpc.default.id}
+  vpc_id = data.aws_vpc.default.id
   tags = {
     Tier = "private"
   }
