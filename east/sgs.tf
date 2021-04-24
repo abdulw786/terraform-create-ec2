8262ac2884893df773c7c333322d659d0d0d262e
Sam Smith <sam@example.com>
2021-04-23 20:18:17 -0400

    added ssh

--- east/sgs.tf
+++ east/sgs.tf
@@ -13,6 +13,14 @@ resource "aws_security_group" "default" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
