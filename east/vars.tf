de5201b0a1309deab2b9876c94224b8e80816ee9
Sam Smith <sam@example.com>
2021-04-23 20:32:41 -0400

    added ec2 file

--- east/vars.tf
+++ east/vars.tf
@@ -1,3 +1,20 @@
 variable "domain" {
   default = "tf-test"
 }
+
+variable "ami_image"  {
+  default = "ami-b70554c8"
+}
+
+variable "name" {
+  description = "Name of the Instance"
+  default = "Instance1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "devops"
+}
