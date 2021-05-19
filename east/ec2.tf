77081e991b961414a782cf5f1696bc8b2bd79cff
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2021-05-18 20:39:12 -0400

    Update ec2.tf

--- east/ec2.tf
+++ east/ec2.tf
@@ -4,3 +4,10 @@ resource "aws_instance" "web1" {
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
  }
+
+resource "aws_instance" "web2" {
+  ami = "${var.ami_image}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+ }
