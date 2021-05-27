b31bb17bf7d1187213e9fddaa01c51945dee5542
abdulw786 <47833187+abdulw786@users.noreply.github.com>
2021-05-26 22:59:41 -0400

    Update elb.tf

--- east/elb.tf
+++ east/elb.tf
@@ -1,10 +1,9 @@
 resource "aws_lb" "default" {
-  for_each           = data.aws_subnet_ids.default.ids
   name               = var.app
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.default.id]
-  subnet_ids = [tolist(data.aws_subnet_ids.default.ids)[0], tolist(data.aws_subnet_ids.default.ids)[1]]
+  subnet_ids.        = [tolist(data.aws_subnet_ids.default.ids)[0], tolist(data.aws_subnet_ids.default.ids)[1]]
   enable_deletion_protection = false
 
   access_logs {
