d4c9dd9667ae0c1139f991049546909bbd3a2a3c
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2021-05-26 20:43:08 -0400

    Create alb.tf

---
+++ east/alb.tf
@@ -0,0 +1,18 @@
+resource "aws_lb" "default" {
+  name               = var.app
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.default.id]
+  subnets            = data.aws_subnet.default.id
+  enable_deletion_protection = false
+
+  access_logs {
+    bucket  = aws_s3_bucket.surrendra-terraform2.bucket
+    prefix  = "alb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
